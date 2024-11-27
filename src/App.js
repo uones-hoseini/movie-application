1e372a42451e7e3408db91e80553b4df1ce5700d
Seyed Younes Hosseini <73585746+uones-hoseini@users.noreply.github.com>
2024-11-27 19:51:23 +0330

    add tailwind

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Word</h1>
+      <h1 className='text-2xl text-red-900'>Hello Word</h1>
     </div>
   );
 }
