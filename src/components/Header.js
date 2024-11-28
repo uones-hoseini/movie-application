4ef0d4a6af2d1ca5c3a523416a2da907d6dfa8da
Seyed Younes Hosseini <73585746+uones-hoseini@users.noreply.github.com>
2024-11-28 17:46:10 +0330

    set search icone

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-import React from "react"
+import React, { useEffect, useLayoutEffect, useState } from "react"
 import logo from "../assets/logo.png"
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import userIcone from "../assets/user.png"
+import { IoSearchSharp } from "react-icons/io5"
 function Header() {
+  const [searchInput, setSearchInput] = useState("")
+  const navigate = useNavigate()
   const navigation = [
     {
       lable: "TV Shows",
@@ -13,9 +16,12 @@ function Header() {
       href: "movie",
     },
   ]
+
+  useEffect(() => {
+    navigate(`/search?${searchInput}`)
+  }, [searchInput])
   return (
     <header className="fixed top-0 w-full h-16 bg-neutral-600 bg-opacity-75">
-   
       <div className="container mx-auto px-3 flex items-center h-full">
         <div>
           <img src={logo} alt="logo" width={120} />
@@ -31,13 +37,19 @@ function Header() {
             )
           })}
         </nav>
-        <div className="ml-auto">
-        <div className="w-8 h-8 rounded-full overflow-auto cursor-pointer active:scale-50 transition-all">
-          <img src={userIcone} alt="user icone" width="w-full h-full" />
-        </div>
+        <div className="ml-auto flex items-center gap-5">
+          <form className="flex items-center">
+            <input onChange={(e) => setSearchInput(e.target.value)} type="text" placeholder="Search here..." className="bg-transparent px-4 outline-none border-none " />
+            <button className="text-2xl text-white">
+              <IoSearchSharp />
+            </button>
+          </form>
+
+          <div className="w-8 h-8 rounded-full overflow-auto cursor-pointer active:scale-50 transition-all">
+            <img src={userIcone} alt="user icone" width="w-full h-full" />
+          </div>
         </div>
       </div>
-  
     </header>
   )
 }
