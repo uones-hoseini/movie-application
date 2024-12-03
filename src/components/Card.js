11bbf08928e7100e9497dbac91536a9d387ef94d
Seyed Younes Hosseini <73585746+uones-hoseini@users.noreply.github.com>
2024-12-03 16:54:07 +0330

    add code for send id in url

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { useSelector } from "react-redux"
 import moment from "moment"
+import { Link } from "react-router-dom"
 
 function Card({ data, trending, index }) {
   const imageURL = useSelector((state) => state.movieData.imageURL)
 
   return (
-    <div className="w-full min-w-[230px] max-w-[230px] h-80 overflow-hidden rounded relative">
+    <Link to={"/"+data.media_type+"/"+data.id} className="w-full min-w-[230px] max-w-[230px] h-80 overflow-hidden rounded relative">
       <img src={imageURL + data?.poster_path} alt="" />
       <div className="absolute top-4">{trending && <div className="py-1 px-4 bg-black/60 backdrop-blur-3xl rounded-r-full overflow-hidden">#{index} Trending</div>}</div>
       <div className="absolute bottom-0 h-14 backdrop-blur-3xl w-full bg-black/60 p-2">
@@ -16,7 +17,7 @@ function Card({ data, trending, index }) {
           <p className="bg-black px-1 rounded-full text-xs text-white ">Rating :{Number(data.vote_average).toFixed(1)}</p>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
