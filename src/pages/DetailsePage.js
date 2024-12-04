b66f27d9dafb7b5ccdc94ce0e208a6229df7b179
Seyed Younes Hosseini <73585746+uones-hoseini@users.noreply.github.com>
2024-12-04 19:48:23 +0330

    add net fetch component to DetailePage

--- src/pages/DetailsePage.js
+++ src/pages/DetailsePage.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import useFetchDetails from '../hooks/useFetchDetails'
 
 function DetailsePage() {
+  const params=useParams()
+
+  console.log("params",params)
+const {data}=useFetchDetails(`${params?.explore}/${params?.id}`)
+console.log("data",data)
+
+
   return (
     <div>
       DetailsePage
